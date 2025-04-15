3777e233d6a0d81e97fcb0d6b064f75136eceda7
EC2 Default User <ec2-user@ip-172-31-72-141.ec2.internal>
2025-04-15 01:29:41 +0000

    add: corrigindo checkov

--- network.tf
+++ network.tf
@@ -3,7 +3,7 @@ resource "aws_vpc" "minha_vpc" {
   instance_tenancy = "default"
 
   tags = {
-    Name = "tf-vpc-hertz-checkov"
+    Name = "tf-vpc-hertz-automation"
   }
 }
 
@@ -172,4 +172,21 @@ resource "aws_nat_gateway" "nat_gw_1b" {
   }
 
   depends_on = [aws_internet_gateway.igw]
+}
+
+# Correcao primeira issue
+resource "aws_flow_log" "example" {
+  log_destination      = "arn:aws:s3:::hertz-clc13-network-terraform-state"
+  log_destination_type = "s3"
+  traffic_type         = "ALL"
+  vpc_id               = aws_vpc.minha_vpc.id
+}
+
+# Correcao segunda issue
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.minha_vpc.id
+  
+  tags = {
+    Name = "my-iac-sg"
+  }
 }
